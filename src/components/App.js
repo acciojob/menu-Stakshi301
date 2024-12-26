5bcbd25f54dd3abbcceb2f98603fab46715a82b0
Stakshi <semwaleishta6@gmail.com>
2024-12-26 17:14:48 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,14 @@
 <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import PageHead from "./components/Page_heading/PageHead"
+import Menu from "./components/Menu/Menu";
+import "./index.css";
+function App() {
+  return (
+    <>
+      <PageHead></PageHead>
+      <Menu></Menu>
+    </>
+  );
+}
 
+export default App;
---
+++ src/components/Page_heading/PageHead.jsx
@@ -0,0 +1,11 @@
+import "./PageHead.css";
+
+const PageHead = () => {
+  return (
+    <>
+      <h1 className="Heading">Our menu</h1>
+    </>
+  );
+};
+
+export default PageHead;
