2d42a96362bda4864948bc9770511677d33501bc
Stakshi <semwaleishta6@gmail.com>
2024-12-26 18:16:45 +0530

    se

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,9 @@
-<p>
-  Now I can render any React component on any DOM node I want using
-  ReactDOM.render
-</p>;
-import PageHead from "./components/Page_heading/PageHead";
-import Menu from "./Menu/Menu";
-import "./index.css";
+import React from "react";
+import Menu from "./components/Menu/Menu";
 function App() {
   return (
     <>
-      <Menu></Menu>
+      <Menu/>
     </>
   );
 }
